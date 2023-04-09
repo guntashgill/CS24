6260f67078370871253ec387cff70ff7e26f8831
Guntash Gill <gillguntash@yahoo.com>
2023-04-09 12:33:06 -0700

    several changes

--- welcome/code/caprot.cpp
+++ welcome/code/caprot.cpp
@@ -1,29 +1,32 @@
 #include <iostream>
 #include <string>
-#include <cstdlib>
 
-using namespace std;
-
-string rotate(string word, int num) {
-    int len = word.length();
-    num = num % len;
-    if (num < 0) {
-        num += len;
-    }
-    return word.substr(len-num) + word.substr(0, len-num);
-}
-
-int main(int argc, char** argv) {
+int main(int argc, char* argv[]) {
     if (argc < 2) {
-        cerr << "Usage: " << argv[0] << " <rotation amount>" << endl;
+        std::cerr << "Usage: caprot <num_chars>" << std::endl;
         return 1;
     }
 
-    int num = atoi(argv[1]);
-    string line;
-    getline(cin, line);
 
-    cout << rotate(line, num) << endl;
+    int num_chars = std::stoi(argv[1]);
+
+
+    std::string line;
+    std::getline(std::cin, line);
+
+    
+    int len = line.length();
+    std::string result;
+    for (int i = 0; i < len; i++) {
+        char c = line[(i + len - num_chars) % len];
+        if (std::islower(c)) {
+            c = std::toupper(c);
+        }
+        result += c;
+    }
+
+    std::cout << result << std::endl;
 
     return 0;
 }
+
