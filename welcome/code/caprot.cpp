6acd5fa78fab50c469910df799af12f74b674005
Guntash Gill <gillguntash@yahoo.com>
2023-04-10 10:32:52 -0700

    several changes

--- welcome/code/caprot.cpp
+++ welcome/code/caprot.cpp
@@ -8,21 +8,45 @@ int main(int argc, char* argv[]) {
     std::string line;
     std::getline(std::cin, line);
 
-    
-    int len = line.length();
-    std::string result = "";
-    result = line;
-    for (int i = 0; i < len; i++) {
-        result[i] = tolower(result[i]);
-    }
-    for (int i = 0; i < len; i++) {
-        if (isupper(line[i])){
-            int p = (i + (atoi(argv[1])) )%len;
-            result[p] = toupper(result[p]); 
+
+    if (atoi(argv[1]) > 0) {
+        int len = line.length();
+        std::string result = "";
+        result = line;
+        for (int i = 0; i < len; i++) {
+            result[i] = tolower(result[i]);
         }
+        for (int i = 0; i < len; i++) {
+            if (isupper(line[i])){
+                int p = (i + (atoi(argv[1])) )%len;
+                result[p] = toupper(result[p]); 
+            }
+        }
+        cout << result << endl;
     }
-    cout << result << endl;
+    else{
+        int len = line.length();
+        std::string result = "";
+        result = string(line);
+        for (int i = 0; i < len; i++) {
+            result[i] = tolower(result[i]);
+        }
+        for (int i = 0; i < len; i++) {
+            if (isupper(line[i])){
+                int p = (i + (atoi(argv[1])));
+                while (p < 0) {
+                    p += len;
+                }
+                result[p] = toupper(result[p]); 
+            }
+        }
+        cout << result << endl;
+    }
 }
 
+
+
+//abCdEfghIjklMnopqrStuvwxYz
+//AbcdEfghijKlmnopQrstUvWxyz
 
 
