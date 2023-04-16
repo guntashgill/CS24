4e4006c2f075ea5c59a1fc63c4b7688de4d92b98
Guntash Gill <gillguntash@yahoo.com>
2023-04-16 12:29:15 -0700

    several changes

--- fibvec/FibVec.cpp
+++ fibvec/FibVec.cpp
@@ -10,5 +10,78 @@ FibVec* create_fibvec() {
   return new FibVec();
 }
 
+static size_t fib(size_t n) {
+    if (n == 0) {
+        return 0;
+    } else if (n == 1) {
+        return 1;
+    } else {
+        return fib(n-1) + fib(n-2);
+    }
+}
+FibVec::FibVec() {
+    data = new int[1];
+    _capacity = 1;
+    count_ = 0;
+}
+FibVec::~FibVec() {
+  delete[] data;
+}
+size_t FibVec::capacity() const {
+  return _capacity;
+}
+size_t FibVec::count() const{
+  return count_;
+}
+void FibVec::resize(size_t new_capacity){
+  int* new_data = new int[new_capacity];
+  for (size_t i = 0; i < count_; i++);{
+    new_data = data;
+  }
+  delete[] data;
+  data = new_data;
+  _capacity = new_capacity;
+}
+
+void FibVec::insert(int value, size_t index){
+  if (index >count_) {
+    throw std::out_of_range("index out of range");
+  }
+  if (count_ == _capacity) {
+    size_t new_capacity = fib(3);
+    while (new_capacity <= count_) {
+      new_capacity = fib(new_capacity +1);
+    }
+    resize(new_capacity);
+  }
+  for (size_t i = count_; i > index; i --) {
+    data[i] = data[i-1];
+  }
+  data[index] = value;
+  count_++;
+}
+
+int FibVec::lookup(size_t index) const{
+  if (index >= count_) {
+    throw std::out_of_range("index out of range");
+  }
+  return data[index];
+}
+
+int FibVec::pop() {
+  if(count_ == 0) {
+    throw std::underflow_error("vector is empty");
+  }
+  count_--;
+  if(count_ <= fib(_capacity-3)){
+    size_t new_capacity = fib(_capacity-2);
+    resize(new_capacity);
+  }
+  return data[count_];
+}
+
+void FibVec::push(int value){
+  insert(value, count_);
+}
 
 // FibVec Function Implementations
--- fibvec/FibVec.h
+++ fibvec/FibVec.h
@@ -3,14 +3,41 @@
 
 // This provides the size_t type:
 #include <cstddef>
+#include <stdexcept>
 
-class FibVec {
+class FibVec {  
+
   // Member Variables
 
   // Helper Functions
 
 public:
-  // Constructor and Destructor
+  FibVec();
+  ~FibVec();
+  //returns the capacity
+  size_t capacity() const; 
+  //returns the #
+  size_t count() const;
+  //inserts a value
+  void insert(int value, size_t index);
+  //returns value at given index
+  int lookup(size_t index) const;
+  //removes and retuns value
+  int pop();
+  //inserts value at the end of the vector
+  void push(int value);
+  //removes and returns value
+  int remove(size_t index);
+
+private:
+  int* data;
+  size_t _capacity;
+  size_t count_;
+  void resize(size_t new_capacity);
+
+
+
+// Constructor and Destructor
 
   // Member Functions
 
