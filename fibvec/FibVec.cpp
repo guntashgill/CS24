7bc2a7b36e1c43346f9282839fcdb47c79478673
Guntash Gill <gillguntash@yahoo.com>
2023-04-18 15:16:59 -0700

    several

--- fibvec/FibVec.cpp
+++ fibvec/FibVec.cpp
@@ -19,57 +19,60 @@ static size_t fib(size_t n) {
         return fib(n-1) + fib(n-2);
     }
 }
+
 FibVec::FibVec() {
     data = new int[1];
     _capacity = 1;
     count_ = 0;
     store_ = 2;
 }
-FibVec::~FibVec() {
-  delete[] data;
+
+FibVec::~FibVec() { 
+    delete[] data;
 }
+
 size_t FibVec::capacity() const {
-  return _capacity;
-}
-size_t FibVec::count() const{
-  return count_;
-}
-void FibVec::resize(size_t new_capacity){
-  int* new_data = new int[new_capacity];
-  for (size_t i = 0; i < count_; i++){
-    new_data[i] = data[i];
-  }
-  delete[] data;
-  data = new_data;
-  _capacity = new_capacity;
+    return _capacity;
 }
 
-void FibVec::insert(int value, size_t index){
-  if (index >count_) {
-    throw std::out_of_range("index out of range");
-  }
-  if (count_ == _capacity) {
-    store_++; 
-    size_t new_capacity = fib(store_);
-    while (new_capacity <= count_) {
-      new_capacity = fib(new_capacity +1);
+size_t FibVec::count() const {
+    return count_;
+}
+
+void FibVec::resize(size_t new_capacity) {
+    int* new_data = new int[new_capacity];
+    for (size_t i = 0; i < count_; i++){
+        new_data[i] = data[i];
     }
-    resize(new_capacity);
-  }
-  for (size_t i = count_; i > index; i--) {
-    data[i] = data[i-1];
-  }
-  data[index] = value;
-  count_++;
+    delete[] data;
+    data = new_data;
+    _capacity = new_capacity;
 }
 
+void FibVec::insert(int value, size_t index) {
+    if (index > count_) { 
+        throw std::out_of_range("index out of range");
+    }
+    if (count_ == _capacity) {
+        store_++;
+        size_t new_capacity = fib(store_);
+        while (new_capacity <= count_) { 
+            new_capacity = fib(new_capacity + 1);
+        }
+        resize(new_capacity);
+    }
+    for (size_t i = count_; i > index; i--) {
+        data[i] = data[i-1];
+    }
+    data[index] = value;
+    count_++;
+}
 
-
-int FibVec::lookup(size_t index) const{
-  if (index >= count_) {
-    throw std::out_of_range("index out of range");
-  }
-  return data[index];
+int FibVec::lookup(size_t index) const {
+    if (index >= count_) {
+        throw std::out_of_range("index out of range");
+    }
+    return data[index];
 }
 
 int FibVec::pop() {
