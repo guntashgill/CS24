0758eaac70cb0fd9c484f976e55a255c4f01629d
Guntash Gill <gillguntash@yahoo.com>
2023-04-18 15:25:09 -0700

    several

--- fibvec/FibVec.cpp
+++ fibvec/FibVec.cpp
@@ -19,84 +19,115 @@ static size_t fib(size_t n) {
         return fib(n-1) + fib(n-2);
     }
 }
-
 FibVec::FibVec() {
     data = new int[1];
     _capacity = 1;
     count_ = 0;
     store_ = 2;
 }
-
-FibVec::~FibVec() { 
-    delete[] data;
+FibVec::~FibVec() {
+  delete[] data;
 }
-
 size_t FibVec::capacity() const {
-    return _capacity;
+  return _capacity;
+}
+size_t FibVec::count() const{
+  return count_;
+}
+void FibVec::resize(size_t new_capacity){
+  int* new_data = new int[new_capacity];
+try {
+  for (size_t i = 0; i < count_; i++){
+    new_data[i] = data[i];
+  }
+} catch (...) {
+  delete[] new_data;
+  throw;
+}
+  for (size_t i = 0; i < count_; i++){
+    new_data[i] = data[i];
+  }
+  delete[] data;
+  data = new_data;
+  _capacity = new_capacity;
 }
 
-size_t FibVec::count() const {
-    return count_;
+void FibVec::insert(int value, size_t index){
+  if (index >count_) {
+    throw std::out_of_range("index out of range");
+  }
+  if (count_ == _capacity) {
+    store_++; 
+    size_t new_capacity = fib(store_);
+    while (new_capacity <= count_) {
+      new_capacity = fib(new_capacity +1);
+    }
+    resize(new_capacity);
+  }
+  for (size_t i = count_; i > index; i--) {
+    data[i] = data[i-1];
+  }
+  data[index] = value;
+  count_++;
 }
 
-void FibVec::resize(size_t new_capacity) {
-    int* new_data = new int[new_capacity];
-    for (size_t i = 0; i < count_; i++){
-        new_data[i] = data[i];
-    }
-    delete[] data;
-    data = new_data;
-    _capacity = new_capacity;
-}
 
-void FibVec::insert(int value, size_t index) {
-    if (index > count_) { 
-        throw std::out_of_range("index out of range");
-    }
-    if (count_ == _capacity) {
-        store_++;
-        size_t new_capacity = fib(store_);
-        while (new_capacity <= count_) { 
-            new_capacity = fib(new_capacity + 1);
-        }
-        resize(new_capacity);
-    }
-    for (size_t i = count_; i > index; i--) {
-        data[i] = data[i-1];
-    }
-    data[index] = value;
-    count_++;
-}
 
-int FibVec::lookup(size_t index) const {
-    if (index >= count_) {
-        throw std::out_of_range("index out of range");
-    }
-    return data[index];
+int FibVec::lookup(size_t index) const{
+  if (index >= count_ || count_ == 0) {
+    throw std::out_of_range("index out of range");
+  }
+  return data[index];
 }
 
 int FibVec::pop() {
-    if (count_ == 0) {
-        throw std::underflow_error("vector is empty");
+if (count_ == 0) {
+  throw std::underflow_error("Underflow.");
+} else if (count_ == 1) {
+  int value = data[0];
+  count_ = 0;
+  resize(1);
+  return value;
+}
+  int value = data[count_ - 1];
+  count_--;
+
+if (count_ == 0) {
+  resize(1);
+} else {
+  bool is_fib = false;
+  int fib_idx = 2; 
+  while (fib(fib_idx) <= count_) {
+    if (fib(fib_idx) == count_) {
+      is_fib = true;
+      break;
     }
-    count_--;
-    if (count_ <= fib(_capacity-3)){
-        size_t new_capacity = fib(_capacity-2);
-        resize(new_capacity);
-    }
-    return data[count_];
+    fib_idx++;
+  }
+
+  if (!is_fib) {
+    size_t new_capacity = fib(fib_idx - 1);
+    resize(new_capacity);
+  }
+}
+
+  return value; 
 }
 
 int FibVec::remove(size_t index) {
-    if (index >= count_){ 
+    if (index >= count_) {
         throw std::out_of_range("Invalid index");
     }
+
     int value = data[index];
+
     for (size_t i = index; i < count_ - 1; i++) {
         data[i] = data[i + 1];
     }
+
     count_--;
-    if (count_ <= _capacity / 2 && _capacity > 1) {
+
+    if (count_ <= _capacity / 2 && _capacity > 2 && count_ <= fib(store_ - 1)) {
         resize(_capacity - 1);
     }
     return value;
