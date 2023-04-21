3edbf37d4a92b1490e98c759aecbbde8384251b0
Guntash Gill <gillguntash@yahoo.com>
2023-04-21 11:19:21 -0700

    hjjlk

--- setree/Node.h
+++ setree/Node.h
@@ -1,6 +1,13 @@
 #ifndef NODE_H
 #define NODE_H
 
+struct Node {
+    std::string value;
+    Node* left;
+    Node* right;
+
+    Node(const std::string& val) : value(val), left(nullptr), right(nullptr) {}
+};
 // Use this file to declare your Node type.
 // Implement Node member functions and helper functions in Node.cpp.
 
--- setree/Set.cpp
+++ setree/Set.cpp
@@ -1,1 +1,81 @@
 #include "Set.h"
+#include <stdexcept>
+
+Set::Set() : mRoot(nullptr) {}
+Set::Set(const Set& other) {
+    mRoot = other.mRoot ? new Node(*other.mRoot) : nullptr;
+}
+Set::Set(Set&& other) {
+    mRoot = other.mRoot;
+    other.mRoot = nullptr;
+}
+Set::~Set() {
+    clear();
+}
+size_t clearHelper(Node* node) {
+    if (!node) {
+        return 0;
+    }
+    size_t count = clearHelper(node->left) + clearHelper(node->right) + 1;
+    delete node;
+    return count;
+}
+
+size_t Set::clear() {
+    size_t count = clearHelper(mRoot);
+    mRoot = nullptr;
+    return count;
+}
+bool Set::contains(const std::string& value) const {
+    Node* node = mRoot;
+    while (node) {
+        if (value < node->value) {
+            node = node->left;
+        } else if (value > node->value) {
+            node = node->right;
+        } else {
+            return true;
+        }
+    }
+    return false;
+}
+size_t countHelper(Node* node) {
+    if (!node) {
+        return 0;
+    }
+    return countHelper(node->left) + countHelper(node->right) + 1;
+}
+
+size_t Set::count() const {
+    return countHelper(mRoot);
+}
+size_t Set::insert(const std::string& value) {
+    if (!mRoot) {
+        mRoot = new Node(value);
+        return 1;
+    }
+
+    Node* parent = nullptr;
+    Node* node = mRoot;
+    while (node) {
+        if (value < node->value) {
+            parent = node;
+            node = node->left;
+        } else if (value > node->value) {
+            parent = node;
+            node = node->right;
+        } else {
+            return 0;
+        }
+    }
+
+    if (value < parent->value) {
+        parent->left = new Node(value);
+    } else {
+        parent->right = new Node(value);
+    }
+
+    return 1;
+}
+
+
