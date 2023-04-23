b2542e5b85371f1a6d202743e728ba626e297f1f
Guntash Gill <gillguntash@yahoo.com>
2023-04-23 12:31:35 -0700

    LOLOLO

--- tictac/move-check.cpp
+++ tictac/move-check.cpp
@@ -6,6 +6,7 @@
 
 int main(int argc, char** argv) {
   bool verbose = false;
+  int exitCode = 0; // Initialize exit code to 0
 
   if(argc == 2 && std::string(argv[1]) == "-v") {
     verbose = true;
@@ -21,7 +22,11 @@ int main(int argc, char** argv) {
   try {
     Move move(line);
     std::cout << move << '\n';
-    return 0;
+
+    // Additional logic to check if move meets expected criteria
+    if (move.row < 1 || move.row > 3 || move.column < 1 || move.column > 3) {
+      exitCode = 1; // Set exit code to 1 if move is not valid
+    }
   }
   catch(const ParseError& e) {
     if(verbose) {
@@ -30,7 +35,9 @@ int main(int argc, char** argv) {
     else {
       std::cout << "Parse error.\n";
     }
-
-    return 1;
+    exitCode = 1; // Set exit code to 1 on parsing error
   }
+
+  return exitCode; // Return the correct exit code
 }
+
