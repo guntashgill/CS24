bc3e2b71777626a2158acdab1174a7bad3b1e374
Guntash Gill <gillguntash@yahoo.com>
2023-04-23 12:38:57 -0700

    LOLOLO

--- tictac/move-check.cpp
+++ tictac/move-check.cpp
@@ -5,13 +5,12 @@
 
 int main(int argc, char** argv) {
   bool verbose = false;
-  int exitCode = 0; // Initialize exit code to 0
 
-  if(argc == 2 && std::string(argv[1]) == "-v") {
+  if (argc == 2 && std::string(argv[1]) == "-v") {
     verbose = true;
   }
 
-  if(verbose) {
+  if (verbose) {
     std::cout << "> ";
   }
 
@@ -21,23 +20,25 @@ int main(int argc, char** argv) {
   try {
     Move move(line);
     std::cout << move << '\n';
-
-    // Additional logic to check if move meets expected criteria
-    if (move.row < 1 || move.row > 3 || move.column < 1 || move.column > 3) {
-      exitCode = 1; // Set exit code to 1 if move is not valid
-    }
-  }
-  catch(const ParseError& e) {
-    if(verbose) {
+    return 0;
+  } catch (const ParseError& e) {
+    if (verbose) {
       std::cout << "Parse error: " << e.what() << '\n';
-    }
-    else {
+    } else {
       std::cout << "Parse error.\n";
     }
-    exitCode = 1; // Set exit code to 1 on parsing error
+
+    return 1;
+  } catch (const InvalidMove& e) {
+    if (verbose) {
+      std::cout << "Invalid move error: " << e.what() << '\n';
+    } else {
+      std::cout << "Invalid move error.\n";
+    }
+
+    return 1;
   }
 
-  return exitCode; // Return the correct exit code
+  // Return 0 on success
+  return 0;
 }
-
-
