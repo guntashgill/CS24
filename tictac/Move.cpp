4cabd3b0625b91366e6c81a7893c6f6fc869c27d
Guntash Gill <gillguntash@yahoo.com>
2023-04-24 17:52:22 -0700

    LOLOLO

--- tictac/Move.cpp
+++ tictac/Move.cpp
@@ -5,47 +5,47 @@
 #include <sstream>
 
 Move::Move(const std::string& input) {
-  // Parse the input string and validate it
+  // Parse the input string to extract move components
   std::istringstream iss(input);
-  int moveNumber;
-  char playerCode;
-  char rowCode;
-  char columnCode;
-  std::string comment;
+  iss >> number;
+  iss >> player;
+  iss >> row;
+  iss >> column;
 
-  if (!(iss >> moveNumber >> std::ws >> playerCode >> std::ws >> rowCode >> columnCode)) {
+
+  if (!(iss >> number >> std::ws >> player >> std::ws >> row >> column)) {
     throw ParseError("Invalid input: " + input);
   }
 
   // Validate move number
-  if (moveNumber < 1 || moveNumber > 9) {
-    throw InvalidMove("Invalid move number: " + std::to_string(moveNumber));
+  if (number < 1 || number > 9) {
+    throw InvalidMove("Invalid move number: " + std::to_string(number));
   }
 
   // Validate player code
-  if (playerCode != 'X' && playerCode != 'O') {
-    throw InvalidMove("Invalid player code: " + std::string(1, playerCode));
+  if (player != 'X' && player != 'O') {
+    throw InvalidMove("Invalid player code: " + std::string(1, player));
   }
 
   // Validate row code
-  if (rowCode < 'A' || rowCode > 'C') {
-    throw InvalidMove("Invalid row code: " + std::string(1, rowCode));
+  if (row < 'A' || row > 'C') {
+    throw InvalidMove("Invalid row code: " + std::string(1, row));
   }
 
   // Validate column code
-  if (columnCode < '1' || columnCode > '3') {
-    throw InvalidMove("Invalid column code: " + std::string(1, columnCode));
+  if (column < '1' || column > '3') {
+    throw InvalidMove("Invalid column code: " + std::string(1, column));
   }
 
   // Extract relevant information from the input and initialize member variables
-  number = moveNumber;
-  player = std::toupper(playerCode);
-  row = std::toupper(rowCode);
-  column = columnCode;
+  number = number;
+  player = std::toupper(player);
+  row = std::toupper(row);
+  column = column;
 }
 
 std::ostream& operator<<(std::ostream& stream, const Move& move) {
   // Generate the output string for the Move object
-  stream << move.number << " " << move.player << " " << move.row << move.column;
+  stream << move.number << " " << move.player << " " << static_cast<char>('A' + move.row - 1) << move.column;
   return stream;
 }
