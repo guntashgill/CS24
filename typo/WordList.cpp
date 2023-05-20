139031e0e90f981ce6d3ed1214dd2e311a466f6c
Guntash Gill <gillguntash@yahoo.com>
2023-05-20 16:15:34 -0700

    Merge branch 'master' of github.com:ucsb-cs24/s23

--- typo/WordList.cpp
+++ typo/WordList.cpp
@@ -1,21 +1,14 @@
 #include "WordList.h"
 #include "Point.h"
 
-// Helper function to calculate the square of a number
-float square(float num) {
-    return num * num;
-}
-
-// Helper function to calculate the square root of a number using Newton's method
-float sqrt(float num) {
-    float x = num;
-    float y = 1.0;
-    float epsilon = 0.000001;
-    while (x - y > epsilon) {
-        x = (x + y) / 2.0;
-        y = num / x;
+// Square root function implementation
+float mySqrt(float x) {
+    // Using Newton's method for square root approximation
+    float guess = x;
+    while (guess * guess - x > 0.001 || x - guess * guess > 0.001) {
+        guess = (guess + x / guess) / 2;
     }
-    return x;
+    return guess;
 }
 
 WordList::WordList(std::istream& stream) {
@@ -38,27 +31,26 @@ Heap WordList::correct(const std::vector<Point>& points, size_t maxcount, float 
     for (const std::string& word : mWords) {
         if (word.length() == points.size()) {
             // Calculate the score for the word using the provided scoring algorithm
-            float score = 0.0;
+            float totalScore = 0.0;
             for (size_t i = 0; i < points.size(); ++i) {
                 float dx = points[i].x - QWERTY[word[i] - 'a'].x;
                 float dy = points[i].y - QWERTY[word[i] - 'a'].y;
-                float distance = sqrt(square(dx) + square(dy));
-                score += 1.0 / (1.0 + distance);
+                float distance = mySqrt(dx * dx + dy * dy);
+                float score = 1.0 / (10.0 * distance * distance + 1.0);
+                totalScore += score;
             }
+            float wordScore = totalScore / static_cast<float>(points.size());
 
-            // Check if the score is above the cutoff and update the heap if necessary
-            if (score >= cutoff) {
+            // Check if the word score is above the cutoff and update the heap if necessary
+            if (wordScore >= cutoff) {
                 if (heap.count() < maxcount) {
-                    heap.push(word, score);
-                } else if (score > heap.top().score) {
+                    heap.push(word, wordScore);
+                } else if (wordScore > heap.top().score) {
                     heap.pop();
-                    heap.push(word, score);
+                    heap.push(word, wordScore);
                 }
             }
         }
     }
     return heap;
 }
-
-
-
