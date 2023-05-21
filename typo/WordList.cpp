d713302442b5288d7c403cdc2ad5d7e917d446ed
Guntash Gill <gillguntash@yahoo.com>
2023-05-21 14:30:45 -0700

    Merge branch 'master' of github.com:ucsb-cs24/s23

--- typo/WordList.cpp
+++ typo/WordList.cpp
@@ -1,9 +1,7 @@
 #include "WordList.h"
 #include "Point.h"
 
-// Square root function implementation
 float mySqrt(float x) {
-    // Using Newton's method for square root approximation
     float guess = x;
     while (guess * guess - x > 0.001 || x - guess * guess > 0.001) {
         guess = (guess + x / guess) / 2;
@@ -11,37 +9,44 @@ float mySqrt(float x) {
     return guess;
 }
 
+bool isValidWord(const std::string& word) {
+    for (char c : word) {
+        if (c < 'a' || c > 'z') {
+            return false;
+        }
+    }
+    return true;
+}
+
 WordList::WordList(std::istream& stream) {
     std::string word;
-    char line[256];
-    while (stream.getline(line, sizeof(line))) {
-        size_t i = 0;
-        while (line[i] != '\0') {
-            if (line[i] < 'a' || line[i] > 'z')
-                break;
-            i++;
+    while (std::getline(stream, word)) {
+        if (isValidWord(word)) {
+            mWords.push_back(word);
         }
-        if (line[i] == '\0')
-            mWords.push_back(line);
     }
+}
+
+float calculateDistance(const Point& p1, const Point& p2) {
+    float dx = p1.x - p2.x;
+    float dy = p1.y - p2.y;
+    return mySqrt(dx * dx + dy * dy);
 }
 
 Heap WordList::correct(const std::vector<Point>& points, size_t maxcount, float cutoff) const {
     Heap heap(maxcount);
     for (const std::string& word : mWords) {
         if (word.length() == points.size()) {
-            // Calculate the score for the word using the provided scoring algorithm
             float totalScore = 0.0;
             for (size_t i = 0; i < points.size(); ++i) {
                 float dx = points[i].x - QWERTY[word[i] - 'a'].x;
                 float dy = points[i].y - QWERTY[word[i] - 'a'].y;
-                float distance = mySqrt(dx * dx + dy * dy);
+                float distance = calculateDistance(points[i], QWERTY[word[i] - 'a']);
                 float score = 1.0 / (10.0 * distance * distance + 1.0);
                 totalScore += score;
             }
             float wordScore = totalScore / static_cast<float>(points.size());
 
-            // Check if the word score is above the cutoff and update the heap if necessary
             if (wordScore >= cutoff) {
                 if (heap.count() < maxcount) {
                     heap.push(word, wordScore);
@@ -54,5 +59,6 @@ Heap WordList::correct(const std::vector<Point>& points, size_t maxcount, float 
     }
     return heap;
 }
+
 
 
