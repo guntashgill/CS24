c84e20b5b66d77234f1ed6cdcf94612e36e815a2
Guntash Gill <gillguntash@yahoo.com>
2023-05-26 10:03:28 -0700

    Merge branch 'master' of github.com:ucsb-cs24/s23

--- wordhop/Dictionary.cpp
+++ wordhop/Dictionary.cpp
@@ -15,7 +15,6 @@ Dictionary* Dictionary::create(std::istream& stream) {
 }
 
 bool Dictionary::isValidWord(const std::string& word) const {
-  // Check if the word is a valid word based on the criteria
   if (word.empty() || !std::all_of(word.begin(), word.end(), ::islower)) {
     return false;
   }
@@ -23,35 +22,22 @@ bool Dictionary::isValidWord(const std::string& word) const {
 }
 
 std::vector<std::string> Dictionary::hop(const std::string& from, const std::string& to) const {
-  // Check if the source and destination words are valid
   if (!isValidWord(from) || !isValidWord(to)) {
     throw std::invalid_argument("Invalid Word");
   }
-
-  // Check if the source and destination words have the same length
   if (from.length() != to.length()) {
     throw std::invalid_argument("Words must have the same length");
   }
-
-  // Check if the source and destination words are the same
   if (from == to) {
     return {from};
   }
-
-  // Create a vector to store the word chain
   std::vector<std::string> wordChain;
   wordChain.push_back(from);
-
-  // Create a vector to keep track of visited words
   std::vector<std::string> visitedWords;
   visitedWords.push_back(from);
-
-  // Perform breadth-first search (BFS) to find a valid word chain
   while (!wordChain.empty()) {
     std::string currentWord = wordChain.front();
     wordChain.erase(wordChain.begin());
-
-    // Find neighboring words that differ by exactly one letter
     for (const std::string& word : wordList) {
       if (word.length() != currentWord.length()) {
         continue;
@@ -75,7 +61,5 @@ std::vector<std::string> Dictionary::hop(const std::string& from, const std::str
       }
     }
   }
-
-  // If no valid chain is found, throw an exception
   throw std::runtime_error("No Chain");
 }
