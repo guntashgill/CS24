7ead3b9b3b5b828c8e50720a56529c7305a45160
Guntash Gill <gillguntash@yahoo.com>
2023-05-30 12:02:35 -0700

    Merge branch 'master' of github.com:ucsb-cs24/s23

--- wordhop/Helpers.cpp
+++ wordhop/Helpers.cpp
@@ -1,8 +1,10 @@
 #include "Helpers.h"
+#include <algorithm>
 
 bool isValidWord(const std::string& word) {
   return !word.empty() && std::all_of(word.begin(), word.end(), [](char c) {
     return std::islower(c);
   });
 }
+
 
