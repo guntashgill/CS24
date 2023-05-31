4057dac3b7c345a33f909ff2dbe5838f4d913b35
Guntash Gill <gillguntash@yahoo.com>
2023-05-31 13:05:54 -0700

    Merge branch 'master' of github.com:ucsb-cs24/s23

--- wordhop/Dictionary.cpp
+++ wordhop/Dictionary.cpp
@@ -4,6 +4,7 @@
 #include <unordered_map>
 #include <unordered_set>
 #include <algorithm>
+#include "Errors.h"
 
 Dictionary::Dictionary(std::istream& stream) {
   std::string entry;
@@ -31,44 +32,45 @@ Dictionary* Dictionary::create(std::istream& stream) {
 Dictionary::~Dictionary() {}
 
 std::vector<std::string> Dictionary::hop(const std::string& from, const std::string& to) {
-  std::vector<std::string> path;
-
-  if (from.size() != to.size()) {
-    return path;
-  }
-  int length = from.size();
-
-  if (dictionary[length].find(from) == dictionary[length].end() ||
-      dictionary[length].find(to) == dictionary[length].end()) {
-    return path;
-  }
-
-  std::unordered_map<std::string, std::string> visited;
-  std::queue<std::string> queue;
-  queue.push(from);
-  visited[from] = "";
-
-  while (!queue.empty()) {
-    std::string current = queue.front();
-    queue.pop();
-
-    if (current == to) {
-      std::string word = current;
-      while (!word.empty()) {
-        path.push_back(word);
-        word = visited[word];
-      }
-      std::reverse(path.begin(), path.end());
-      break;
+    std::vector<std::string> path;
+    
+    if (from.size() != to.size()) {
+        throw InvalidWord("Invalid word.");
     }
-
-    for (const auto& neighbor : dictionary[length][current]) {
-      if (visited.find(neighbor) == visited.end()) {
-        visited[neighbor] = current;
-        queue.push(neighbor);
-      }
+    
+    int length = from.size();
+    
+    if (dictionary[length].find(from) == dictionary[length].end() ||
+        dictionary[length].find(to) == dictionary[length].end()) {
+        throw InvalidWord("Invalid word.");
     }
-  }
-
-  return path;
+    
+    std::unordered_map<std::string, std::string> visited;
+    std::queue<std::string> queue;
+    queue.push(from);
+    visited[from] = "";
+    
+    while (!queue.empty()) {
+        std::string current = queue.front();
+        queue.pop();
+        
+        if (current == to) {
+            std::string word = current;
+            while (!word.empty()) {
+                path.push_back(word);
+                word = visited[word];
+            }
+            std::reverse(path.begin(), path.end());  // Reverse the path to get the correct order
+            return path;
+        }
+        
+        for (const auto& neighbor : dictionary[length][current]) {
+            if (visited.find(neighbor) == visited.end()) {
+                visited[neighbor] = current;
+                queue.push(neighbor);
+            }
+        }
+    }
+    
+    throw NoChain();
 }
