a84bc84943deca7acef550493c9e9c14ffdd6d0b
Guntash Gill <gillguntash@yahoo.com>
2023-06-09 09:46:29 -0700

    asdjkhfals

--- tictac/Board.h
+++ tictac/Board.h
@@ -5,28 +5,16 @@
 
 class Board {
 public:
-  // Default constructor
   Board();
-
-  // Apply a move to the board
-  void applyMove(const Move& move);
-
-  // Check if the game is over
+  void gameMove(const Move& move);
   bool isGameOver();
-
-  // Get the current player
-  char getCurrentPlayer();
+  void gameResult();
+  bool hasPlayerWon(char player);
 
 private:
-  // Member Variables
-  char grid[3][3]; // Represents the game board
-  char currentPlayer; // Represents the current player ('X' or 'O')
-
-  // Helper function to check if a player has won
-  bool checkWin(char player);
-
-  // Helper function to check if the board is full
-  bool isBoardFull();
+  char boardCells[3][3];
+  char playerTurn;
+  int moveNumber;
 };
 
 #endif
--- tictac/game-check.cpp
+++ tictac/game-check.cpp
@@ -1,1 +1,28 @@
+#include "Move.h"
+#include "Errors.h"
+#include "Board.h"
+#include <iostream>
+#include <string>
+
+int main() {
+  Board board;
+  std::string input;
+  bool validMoves = true;
+
+  while (std::getline(std::cin, input)) {
+    try {
+      Move move(input);
+      board.gameMove(move);
+    } catch (const ParseError& error) {
+      std::cerr << "Parse error." << std::endl;
+      return 1;
+    } catch (const InvalidMove& error) {
+      std::cerr << "Invalid move." << std::endl;
+      return 2;
+    }
+  }
+
+  board.gameResult();
+  return 0;
+}
 
